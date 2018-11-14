6f0be223dd4eb3f4cbc2acf84319014bb8805126
unknown <magnusbarros@yahoo.com>
2018-11-14 19:58:59 -0200

    comm

--- animal/zoologico.ts
+++ animal/zoologico.ts
@@ -4,5 +4,23 @@ export default class zoo{
     private endereco: string
     private listaDeAnimais: Array<animal>
 
+    /*Todo parâmetro e/ou atributo
+    definido no início do meu método
+    ou função só poderá ser acessado
+    por ele mesmo
+    */
+   
+    constructor(end: string,
+        list: Array<animal>){
+            this.endereco = end
+            this.listaDeAnimais = list
+        }
 
+        public mostrarEndereco(): string{
+            return this.endereco
+        }
+
+        public mostrarAnimais(): Array<animal>{
+            return this.listaDeAnimais
+        }
 }
